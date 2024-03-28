c8ea65846f83e94b53bd86a760ae1a979f579ec6
Vedant Akare <81575978+ve703@users.noreply.github.com>
2024-03-28 12:25:29 +0530

    Add VJTI logo and components directory

---
+++ src/components/Navbar.js
@@ -0,0 +1,176 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import HelpIcon from "@mui/icons-material/Help";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import AccountTreeIcon from "@mui/icons-material/AccountTree";
+import CorporateFareIcon from "@mui/icons-material/CorporateFare";
+import SignalCellularAltIcon from "@mui/icons-material/SignalCellularAlt";
+import PeopleIcon from "@mui/icons-material/People";
+import MenuIcon from "@mui/icons-material/Menu";
+import SchoolIcon from "@mui/icons-material/School";
+import "./Navbar.css";
+import HomeIcon from "@mui/icons-material/Home";
+import PermContactCalendarIcon from "@mui/icons-material/PermContactCalendar";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {/* {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))} */}
+        <ListItem>
+          <ListItemButton>
+            <ListItemIcon>
+              <SchoolIcon />
+            </ListItemIcon>
+            <ListItemText primary={"VJTI"} />
+          </ListItemButton>
+        </ListItem>
+        <Divider />
+        <ListItem disablePadding>
+          <ListItemButton component={Link} to="/">
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Home"} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component={Link} to="/ourTeam">
+            <ListItemIcon>
+              <PeopleIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Our Team"} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <HelpIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Why VJTI"} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <SignalCellularAltIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Placement Statistics"} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <AccountTreeIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Recruitment Process"} />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <CorporateFareIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Our Recruiters"} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component={Link} to="/contact">
+            <ListItemIcon>
+              <PermContactCalendarIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Contact Us"} />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <ExitToAppIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Sign Up"} />
+          </ListItemButton>
+        </ListItem>
+      </List>
+
+      <Divider />
+      <List>
+        <ListItem disablePadding>
+          <ListItemText
+            primary={"Copyright ©2024 VJTI Mumbai.All Rights Reserved"}
+          />
+        </ListItem>
+      </List>
+    </Box>
+  );
+
+  return (
+    <div className="Navbar">
+      {/* {['left', 'right', 'top', 'bottom'].map((anchor) => (
+                <React.Fragment key={anchor}>
+                    <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+                    <Drawer
+                        anchor={anchor}
+                        open={state[anchor]}
+                        onClose={toggleDrawer(anchor, false)}
+                    >
+                        {list(anchor)}
+                    </Drawer>
+                </React.Fragment>
+            ))} */}
+
+      <MenuIcon onClick={toggleDrawer("left", true)} />
+
+      <Drawer
+        anchor={"left"}
+        open={state["left"]}
+        onClose={toggleDrawer("left", false)}
+      >
+        {list("left")}
+      </Drawer>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/OurTeam.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OurTeam = () => {
+  return <div>Key Contributors</div>;
+};
+
+export default OurTeam;
