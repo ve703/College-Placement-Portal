80d2a225aab930e9245133d0056b7faccdd1a4df
Vedant Akare <81575978+ve703@users.noreply.github.com>
2024-04-09 00:14:53 +0530

    Resolve conflicts and update files

--- src/components/EducationalDetails.js
+++ src/components/EducationalDetails.js
@@ -1,155 +1,173 @@
-import React from 'react'
-import vedu from './vedu.jpg'
+import React from "react";
+import sample_profile from "./sample_profile.jpg";
 
-import Box from '@mui/material/Box';
-import { Avatar, ListItem, Stack,List, ListItemText, ListItemButton, Divider } from '@mui/material';
+import Box from "@mui/material/Box";
 import {
-    createTheme,
-    responsiveFontSizes,
-    ThemeProvider,
-} from '@mui/material/styles';
-import Typography from '@mui/material/Typography';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-  
+  Avatar,
+  ListItem,
+  Stack,
+  List,
+  ListItemText,
+  ListItemButton,
+  Divider,
+} from "@mui/material";
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
 let theme = createTheme({
-    typography: {
-      // Tell MUI what the font-size on the html element is.
-      body1: {
-        fontWeight: 100,
-        fontSize:25,
-      },
-      subtitle1: {
-        fontSize: 10,
-        fontStyle: 'italic',
-      },
+  typography: {
+    // Tell MUI what the font-size on the html element is.
+    body1: {
+      fontWeight: 100,
+      fontSize: 25,
     },
-  });
-  theme = responsiveFontSizes(theme);
-  
-  function createData(Certificates, Specialization, Institute, PassingYear, BoardUniversity,Percentage) {
-    return { Certificates, Specialization, Institute, PassingYear, BoardUniversity,Percentage };
-  }
-  
-  const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0,100),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3,100),
-    createData('Eclair', 262, 16.0, 24, 6.0,100),
-    createData('Cupcake', 305, 3.7, 67, 4.3,100),
-    createData('Gingerbread', 356, 16.0, 49, 3.9,100),
-  ];
+    subtitle1: {
+      fontSize: 10,
+      fontStyle: "italic",
+    },
+  },
+});
+theme = responsiveFontSizes(theme);
 
+function createData(
+  Certificates,
+  Specialization,
+  Institute,
+  PassingYear,
+  BoardUniversity,
+  Percentage
+) {
+  return {
+    Certificates,
+    Specialization,
+    Institute,
+    PassingYear,
+    BoardUniversity,
+    Percentage,
+  };
+}
 
-  function createData1(Semester, Backlogs, PassingYear,Percentage) {
-    return { Semester, Backlogs, PassingYear,Percentage };
-  }
-  
-  const rows1 = [
-    createData1('First-Semester', 159, 6.0, 24, 4.0,100),
-    createData1('Second-Semester', 237, 9.0, 37, 4.3,100),
-    createData1('Third-Semester', 262, 16.0, 24, 6.0,100),
-    createData1('Fourth-Semister', 305, 3.7, 67, 4.3,100),
-    createData1('Fifth-Semestre', 356, 16.0, 49, 3.9,100),
-    createData1('sixth-Semestre', 356, 16.0, 49, 3.9,100),
-    createData1('seventh-Semestre', 356, 16.0, 49, 3.9,100),
-    createData1('eightth-Semestre', 356, 16.0, 49, 3.9,100),
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0, 100),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3, 100),
+  createData("Eclair", 262, 16.0, 24, 6.0, 100),
+  createData("Cupcake", 305, 3.7, 67, 4.3, 100),
+  createData("Gingerbread", 356, 16.0, 49, 3.9, 100),
+];
 
-  ];
-  
+function createData1(Semester, Backlogs, PassingYear, Percentage) {
+  return { Semester, Backlogs, PassingYear, Percentage };
+}
 
+const rows1 = [
+  createData1("First-Semester", 159, 6.0, 24, 4.0, 100),
+  createData1("Second-Semester", 237, 9.0, 37, 4.3, 100),
+  createData1("Third-Semester", 262, 16.0, 24, 6.0, 100),
+  createData1("Fourth-Semister", 305, 3.7, 67, 4.3, 100),
+  createData1("Fifth-Semestre", 356, 16.0, 49, 3.9, 100),
+  createData1("sixth-Semestre", 356, 16.0, 49, 3.9, 100),
+  createData1("seventh-Semestre", 356, 16.0, 49, 3.9, 100),
+  createData1("eightth-Semestre", 356, 16.0, 49, 3.9, 100),
+];
 
 const EducationalDetails = () => {
   return (
-    <Box sx={{m:2}}>
-        
-            <Stack direction="row" spacing={2} sx={{width:500,height:100}}>
-                <Avatar sx={{width:80,height:80,border: '2px solid blue'}} src={vedu}/>
-                    <div>
-                    <ThemeProvider theme={theme}>
-                        <Typography variant="body1">vedant bhu</Typography>
-                        <Divider/>
-                        <Typography variant="subtitle1">@gmail.com</Typography>
-                    </ThemeProvider>
-                    </div>
-            </Stack>
+    <Box sx={{ m: 2 }}>
+      <Stack direction="row" spacing={2} sx={{ width: 500, height: 100 }}>
+        <Avatar
+          sx={{ width: 80, height: 80, border: "2px solid blue" }}
+          src={sample_profile}
+        />
+        <div>
+          <ThemeProvider theme={theme}>
+            <Typography variant="body1">vedant bhu</Typography>
+            <Divider />
+            <Typography variant="subtitle1">@gmail.com</Typography>
+          </ThemeProvider>
+        </div>
+      </Stack>
 
-            <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Certificates/Degree</TableCell>
-            <TableCell align="right">Specialization</TableCell>
-            <TableCell align="right">Institute</TableCell>
-            <TableCell align="right">Passing Year</TableCell>
-            <TableCell align="right">Board/University</TableCell>
-            <TableCell align="right">Percentage</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.Certificates}
-              </TableCell>
-              <TableCell align="right">{row.Specialization}</TableCell>
-              <TableCell align="right">{row.Institute}</TableCell>
-              <TableCell align="right">{row.PassingYear}</TableCell>
-              <TableCell align="right">{row.BoardUniversity}</TableCell>
-              <TableCell align="right">{row.Percentage}</TableCell>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Certificates/Degree</TableCell>
+              <TableCell align="right">Specialization</TableCell>
+              <TableCell align="right">Institute</TableCell>
+              <TableCell align="right">Passing Year</TableCell>
+              <TableCell align="right">Board/University</TableCell>
+              <TableCell align="right">Percentage</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.Certificates}
+                </TableCell>
+                <TableCell align="right">{row.Specialization}</TableCell>
+                <TableCell align="right">{row.Institute}</TableCell>
+                <TableCell align="right">{row.PassingYear}</TableCell>
+                <TableCell align="right">{row.BoardUniversity}</TableCell>
+                <TableCell align="right">{row.Percentage}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
 
-    <Typography align='left'>Semister Info :</Typography>
-    <Divider/>
-    <Divider/>
-    <Divider/>
-    <Divider/>
-    <Divider/>
-    <Divider/>
-    <Divider/>
+      <Typography align="left">Semister Info :</Typography>
+      <Divider />
+      <Divider />
+      <Divider />
+      <Divider />
+      <Divider />
+      <Divider />
+      <Divider />
 
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Semester</TableCell>
-            <TableCell align="right">Backlogs</TableCell>
-            <TableCell align="right">Passing Year</TableCell>
-            <TableCell align="right">Percentage</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows1.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.Semester}
-              </TableCell>
-              <TableCell align="right">{row.Backlogs}</TableCell>
-              <TableCell align="right">{row.PassingYear}</TableCell>
-              <TableCell align="right">{row.Percentage}</TableCell>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Semester</TableCell>
+              <TableCell align="right">Backlogs</TableCell>
+              <TableCell align="right">Passing Year</TableCell>
+              <TableCell align="right">Percentage</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-    
-        
+          </TableHead>
+          <TableBody>
+            {rows1.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.Semester}
+                </TableCell>
+                <TableCell align="right">{row.Backlogs}</TableCell>
+                <TableCell align="right">{row.PassingYear}</TableCell>
+                <TableCell align="right">{row.Percentage}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Box>
-  )
-}
+  );
+};
 
-export default EducationalDetails
+export default EducationalDetails;
--- src/components/StudentInfo.js
+++ src/components/StudentInfo.js
@@ -1,73 +1,80 @@
-import React from 'react'
+import React from "react";
 
-import PropTypes from 'prop-types';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import { Avatar, ListItem, Stack,List, ListItemText, ListItemButton, Divider } from '@mui/material';
-import vedu from './vedu.jpg';
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import {
+  Avatar,
+  ListItem,
+  Stack,
+  List,
+  ListItemText,
+  ListItemButton,
+  Divider,
+} from "@mui/material";
+import sample_profile from "./sample_profile.jpg";
 
-import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import EducationalDetails from './EducationalDetails';
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import EducationalDetails from "./EducationalDetails";
 
-        function CustomTabPanel(props) {
-            const { children, value, index, ...other } = props;
-        
-            return (
-            <div
-                role="tabpanel"
-                hidden={value !== index}
-                id={`simple-tabpanel-${index}`}
-                aria-labelledby={`simple-tab-${index}`}
-                {...other}
-            >
-                {value === index && (
-                <Box sx={{ p: 3 }}>
-                    <Typography>{children}</Typography>
-                </Box>
-                )}
-            </div>
-            );
-        }
-  
-        CustomTabPanel.propTypes = {
-            children: PropTypes.node,
-            index: PropTypes.number.isRequired,
-            value: PropTypes.number.isRequired,
-        };
-  
-  function a11yProps(index) {
-    return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-    };
-  }
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
 
-  const array=[
-    "RollNo. :",
-    "Program :",
-    "Stream :",
-    "Enrollment Year :",
-    "Backlog :",
-    "Current Percentage :",
-    "Email :",
-    "Phone No :",
-    "Sex :",
-    "D.O.B :",
-    "Current Address :",
-    "permanent Address :"
-    ];
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
+const array = [
+  "RollNo. :",
+  "Program :",
+  "Stream :",
+  "Enrollment Year :",
+  "Backlog :",
+  "Current Percentage :",
+  "Email :",
+  "Phone No :",
+  "Sex :",
+  "D.O.B :",
+  "Current Address :",
+  "permanent Address :",
+];
 
 const StudentInfo = () => {
-    const [value, setValue] = React.useState(0);
+  const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
-
 
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
@@ -77,46 +84,51 @@ const StudentInfo = () => {
   const handleClose = () => {
     setAnchorEl(null);
   };
-  
 
   return (
     <Box>
-      <Typography variant='h6'>My Profile</Typography>
-      <Box sx={{ width: '100%',border: 1}} >
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+      <Typography variant="h6">My Profile</Typography>
+      <Box sx={{ width: "100%", border: 1 }}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="basic tabs example"
+          >
             <Tab label="Personal Details" {...a11yProps(0)} />
             <Tab label="Educational Details" {...a11yProps(1)} />
             <Tab label="Resume" {...a11yProps(2)} />
           </Tabs>
         </Box>
-      <CustomTabPanel value={value} index={0}>
+        <CustomTabPanel value={value} index={0}>
           <Stack direction="row" spacing={10}>
-                  <Avatar sx={{width:150,height:150,border: '2px solid blue'}} alt='Profile pic' src={vedu}/>
-                  <Box sx={{border: 1 }}>
-                  <Typography variant='h5'>Vedant Bhu  </Typography>
-                  <Divider/>
-                      <List sx={{height:600,width:980,background:"white"}}>
-                          {array.map((listElem)=>(
-                              <ListItem >
-                                      <ListItemText primary={listElem}/>
-                              </ListItem>
-                          ))}
-                      </List>
-                  </Box>
+            <Avatar
+              sx={{ width: 150, height: 150, border: "2px solid blue" }}
+              alt="Profile pic"
+              src={sample_profile}
+            />
+            <Box sx={{ border: 1 }}>
+              <Typography variant="h5">Student Name </Typography>
+              <Divider />
+              <List sx={{ height: 600, width: 980, background: "white" }}>
+                {array.map((listElem) => (
+                  <ListItem>
+                    <ListItemText primary={listElem} />
+                  </ListItem>
+                ))}
+              </List>
+            </Box>
 
-                  <Button
-                        sx={{height:20}}
-                        id="basic-button"
-                        aria-controls={open ? 'basic-menu' : undefined}
-                        aria-haspopup="true"
-                        aria-expanded={open ? 'true' : undefined}
-                        onClick={handleClick}
-
-
-                  >
-                      <Typography  variant='h5'>...</Typography>
-                  </Button>
+            <Button
+              sx={{ height: 20 }}
+              id="basic-button"
+              aria-controls={open ? "basic-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
+            >
+              <Typography variant="h5">...</Typography>
+            </Button>
 
             <Menu
               id="basic-menu"
@@ -124,25 +136,23 @@ const StudentInfo = () => {
               open={open}
               onClose={handleClose}
               MenuListProps={{
-                'aria-labelledby': 'basic-button',
+                "aria-labelledby": "basic-button",
               }}
             >
-                  <MenuItem onClick={handleClose}>Edit Profile</MenuItem>
-                  <MenuItem onClick={handleClose}>Change Password</MenuItem>
+              <MenuItem onClick={handleClose}>Edit Profile</MenuItem>
+              <MenuItem onClick={handleClose}>Change Password</MenuItem>
             </Menu>
           </Stack>
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={1}>
-          <EducationalDetails/>
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={2}>
-        Resume
-      </CustomTabPanel>
-
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={1}>
+          <EducationalDetails />
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={2}>
+          Resume
+        </CustomTabPanel>
       </Box>
-  </Box>
-                   
-  )
-}
+    </Box>
+  );
+};
 
-export default StudentInfo
+export default StudentInfo;
